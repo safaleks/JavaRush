354e4eccdbe08a02b6046a1084cb6c6ddeacc8ff
safaleks <safaleks@mail.ru>
2023-11-23 23:30:21 +0300

    Добавлена таска 8

---
+++ src/Eighth_programm_JR/Main.java
@@ -0,0 +1,16 @@
+package Eighth_programm_JR;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+//        int a = 10;
+        int b = 6;
+//        int b = 12;
+//        int sum = 1 + a + b;
+//        int sum = 2 + a + b;
+        int sum = 3 + a + b;
+//        int sum = 4 + a + b;
+
+        System.out.println("sum = " + sum);
+    }
+}
